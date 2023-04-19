b5ba7b4930047adfe9a6324e07fe798444a74838
Farasat Khan <farasatkahan@gmail.com>
2023-04-19 16:09:05 +0500

    added auth router to server.js

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('./src/api/v1/services/database');
+
+var AuthRouter = require('./src/api/v1/routes/Auth');
 
 var app = express();
 
@@ -19,8 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', AuthRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
