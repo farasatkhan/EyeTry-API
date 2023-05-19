8463801c8bfaa77ef97b58b74f42c4492716df8f
Farasat Khan <farasatkahan@gmail.com>
2023-05-19 18:51:30 +0500

    Changed Mongodb URL from localhost to 127.0.0.1

--- src/api/v1/services/database/index.js
+++ src/api/v1/services/database/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose
-.connect('mongodb://localhost:27017/eyetry')
+.connect('mongodb://127.0.0.1:27017/eyetry')
 .then(() => console.log("Database Connected."))
 .catch((err) => console.log(err));
