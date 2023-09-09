0560ea43c66f0e26040a5ae30a449c65f487616d
Alliyan <alliyan732@gmail.com>
2023-09-09 17:31:00 +0500

    order schema added

--- src/api/v1/models/Order.js
+++ src/api/v1/models/Order.js
@@ -1,11 +1,52 @@
 const mongoose = require('mongoose');
 
-// baqi ka schema banana hy khud..
 const OrderSchema = new mongoose.Schema({
-    order_no : {
-        type: String,
+    order_no: {
+        type: String,       // IDK about it, Farasat added it before
         required: true
-    }
+    },
+    customer: {           // user buying the product, refferencing to User schema
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    frame: {              // selected frame, refferencing to Frames schema
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Frames',
+        required: true,
+    },
+    prescription: {       // refferencing to prescription schema
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Prescription',
+        required: true
+    },
+    prescriptionType: {   // prescription , non prescription, readers
+        type: String,
+        required: true,
+    },
+    packageType: {        // Silver, Gold, Platinum, Diamond 
+        type: String,
+        required: true,
+    },
+    lensType: {            // clear, blue light, sunglasses , Transition.. etc
+        type: [String],
+        required: true
+    },
+    upgrades: {           // includes additional coatings and customizations eg, Super Hydrophobic, color tinit text engraving etc
+        type: [String],
+    },
+    lensCoating: {        // Lenses, Anti-scratch coating, 100% UV-Block coating, Anti-reflective coating etc
+        type: [String],
+        required: true,
+    },
+    totalPrice: {         // total order price
+        type: Number,
+        required: true,
+    },
+    orderDate: {        // order date
+        type: Date,
+        default: Date.now,
+    },
 });
 
 const Order = mongoose.model('Order', OrderSchema);
