1f6041def3c4250b817313ba95641aa8b38c3145
Farasat Khan <farasatkahan@gmail.com>
2023-10-13 16:16:46 +0500

    admin can now uploaded unlimited images

--- src/api/v1/routes/Products/Glasses.js
+++ src/api/v1/routes/Products/Glasses.js
@@ -11,7 +11,7 @@ router.get('/:glassesId', GlassesController.viewParticularGlasses);
 router.put('/:glassesId', GlassesController.updateGlasses);
 router.delete('/:glassesId', GlassesController.deleteGlasses);
 
-router.put('/:glassesId/images', uploadProductImagesServer.array('product_images', 5), GlassesController.addProductImages);
+router.put('/:glassesId/images', uploadProductImagesServer.array('product_images'), GlassesController.addProductImages);
 router.get('/:glassesId/images' , GlassesController.viewProductImages);
 router.delete('/:glassesId/images/:imageId', GlassesController.deleteProductImages);
 
