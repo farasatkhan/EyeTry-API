dd7d404647607e72ca1c39069b53ffc8d1f017fd
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-11-27 09:20:37 +0500

    add message controller to handle messages

---
+++ src/api/v1/controllers/Chat/MessageController.js
@@ -0,0 +1,32 @@
+const MessageModel = require('../../models/Message')
+
+exports.addMessage = async (req,res)=>{
+    const {chatId,senderId,text} = req.body
+    const message = new MessageModel({
+        chatId,
+        senderId,
+        text
+    })
+
+    try{
+        const result = await message.save();
+        res.status(200).json(result)
+    }catch(e){
+        res.status(500).json(e)
+    }
+}
+
+exports.getMessages = async (req,res)=>{
+    const {chatId} = req.params
+
+    try{
+        const result = await MessageModel.find({
+            chatId
+        });
+        res.status(200).json(result)
+    }catch(e){
+        res.status(500).json(e)
+    }
+}
+
+
